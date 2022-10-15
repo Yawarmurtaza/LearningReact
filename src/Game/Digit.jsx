9f05fcbdd16fb229cfe2b1f877b83003d4603db4
yawarmurtaza <yawar_yawar@hotmail.com>
2022-10-14 17:57:52 -0700

    Converting StartMatch to a function component instead of class, the setState, state hook is working. Game is in working condition.

--- src/Game/Digit.jsx
+++ src/Game/Digit.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import { GetColour } from "./Utils";
 
-export default class Digit extends React.Component {
-    
-    constructor(props) {
-        super(props);
+export default function Digit(props) {
+
+    function DigitClickHandler(number, status) {
+        props.onNumberClick(number, status);
     }
 
-    DigitClickHandler(number, status){        
-        this.props.onNumberClick(number, status);
-    }
-    
-    render(){
-        return (
-            <button 
-                className="number" 
-                onClick={this.DigitClickHandler(this.props.digit, this.props.numberStatus)}
-                style={{background: GetColour(this.props.numberStatus)}}
-                >
-                {this.props.digit}
-            </button>
-        );
-    }
+
+    return (
+        <button
+            className="number"
+            onClick={() => props.onNumberClick(props.digit, props.numberStatus)} // notice here the call to the function passed in from parent component.
+            style={{ background: GetColour(props.numberStatus) }}
+        >
+            {props.digit}
+        </button>
+    );
+
 }
--- src/Game/StartMatch.jsx
+++ src/Game/StartMatch.jsx
@@ -1,11 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Digit from "./Digit";
 import DisplayStars from "./DisplayStars";
 import "./Game.css";
-import { GenerateRandom, CreateArray, SumArray} from "./Utils";
+import { GenerateRandom, CreateArray, SumArray, RandomSumIn } from "./Utils";
 
+export default function StartMatch(props) {
+    const totalNumbers = 9;
+    const [starCount, setStarCount] = useState(GenerateRandom(1, totalNumbers));
+    const [availableNumbers, setAvailableNumbers] = useState(CreateArray(1, totalNumbers));
+    const [candidiateNumbers, setCandidiateNumbers] = useState([]);
+
+    function numberStatus(number) {
+        if (!availableNumbers.includes(number)) {
+            return "used";
+        }
+        if (candidiateNumbers.includes(number)) {
+            // sum of candidates array should be less of equal to number of stars which would mean the 'number' is a possible candidate.
+            const wrongCandidates = SumArray(candidiateNumbers) > starCount;
+            return wrongCandidates ? "wrong" : "candidate";
+        }
+        return "available";
+    }
+
+    function onNumberClick(number, status) {        
+
+        if (status === "used") {
+            return;
+        }
+
+         const newCandidates = candidiateNumbers.concat(number);
+        //const newCandidates = this.setState({ "candidiateNumbers": [...this.state.candidiateNumbers, number] });
+
+        if (SumArray(newCandidates) !== starCount) {
+            // this.setState({"setCandidiateNumbers" : newCandidates});
+            setCandidiateNumbers(newCandidates);
+
+        } else {
+            // we have a correct pick!
+            const newAvailableNums = availableNumbers.filter(n => !newCandidates.includes(n));
+            setStarCount(RandomSumIn(newAvailableNums, 9)); // redraw number of stars that are playable.
+            setAvailableNumbers(newAvailableNums);
+            setCandidiateNumbers([]);
+        }
+    }
+
+
+    return (
+        <div className="game">
+            <div className="help">
+                Pick 1 or more numbers that sum to the number of stars
+            </div>
+
+            <div className="body">
+                <div className="left">
+                    <DisplayStars totalStars={starCount} />
+                </div>
+                <div className="right">
+                    {
+                        CreateArray(1, totalNumbers).map(number =>
+                            <Digit key={number}
+                                digit={number}
+                                numberStatus={numberStatus(number)}
+                                onNumberClick={onNumberClick} />
+                        )}
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+/*
 export default class StartMatch extends React.Component {
-
+    numberCount = 9;
     constructor(props) {
         super(props);
         this.state = { 
@@ -52,7 +119,7 @@ export default class StartMatch extends React.Component {
 
     }
 
-    numberCount = 9;
+    
    
 
     render() {
@@ -80,4 +147,4 @@ export default class StartMatch extends React.Component {
             </div>
         );
     }
-}
+}*/
